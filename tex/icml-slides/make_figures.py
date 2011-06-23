5f2541231d12ac96905cbd4b3b0daec35be3d424
tdhock (Toby Dylan Hocking) <tdhock5@gmail.com>
2011-06-23 21:25:45 +0000

    ANIMATION(file) tag
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clusterpath@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tex/icml-slides/make_figures.py
+++ tex/icml-slides/make_figures.py
@@ -1,7 +1,21 @@
-template = open("2011-07-01-HOCKING-icml-clusterpath-slides-in.Rnw").read()
-slide_tmp = open("panels.Rnw").read()
-slides = [slide_tmp%(s,s) for s in [1,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1,0]]
-slides_sweave = '\n'.join(slides)
-sweave = template.replace("COMPARE_NORM_WEIGHTS",slides_sweave)
-f=open("2011-07-01-HOCKING-icml-clusterpath-slides.Rnw","w")
-f.write(sweave)
+def process_commands(text):
+    "Scan text for animation commands and process any found."
+    chunks=text.split("ANIMATION(")
+    results=chunks[0]
+    for chunk in chunks[1:]:
+        pieces=chunk.split(")")
+        f=pieces[0]+".Rnw"
+        result=process_template(f)
+        results += result+")".join(pieces[1:])
+    return results
+def process_template(f):
+    "Scan template file for %s and fill in."
+    lines=open(f).readlines()
+    items=lines[0].replace("%","").split()
+    text="".join(lines[1:])
+    slides=[text.replace("%s",i) for i in items]
+    return '\n'.join(slides)
+if __name__ == "__main__":
+    commands = open("2011-07-01-HOCKING-icml-clusterpath-slides.Ranim").read()
+    f=open("2011-07-01-HOCKING-icml-clusterpath-slides.Rnw","w")
+    f.write(process_commands(commands))
