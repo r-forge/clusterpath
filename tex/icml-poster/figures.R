27522c78b3dea60d524dbb8e49a2ccdb134d1903
tdhock (Toby Dylan Hocking) <tdhock5@gmail.com>
2011-06-26 15:53:47 +0000

    l1 figures
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clusterpath@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tex/icml-poster/figures.R
+++ tex/icml-poster/figures.R
@@ -11,6 +11,9 @@ path <- clusterpath.l1.id(pts[,1:2])
 path2d <- castbreakpoints(path)
 cutline <- 0.18
 pred <- subset(castbreakpoints(predict(path,c(0,cutline))),lambda>0)
+mytheme <-   opts(axis.title.x=theme_text(vjust=-0.5),
+  axis.text.x=theme_text(vjust=-0.5),
+  axis.ticks.margin=unit(0.55,"lines"))
 p <- ggplot(pts,aes(alpha.2,alpha.1))+
   geom_path(aes(group=row),data=path2d)+
   geom_point(subset=.(row==10))+
@@ -18,20 +21,29 @@ p <- ggplot(pts,aes(alpha.2,alpha.1))+
   geom_point(data=pred,colour="red")+
   coord_equal()+
   opts(title="$\\ell_1$ clusterpath of 10 points in 2d")+
-  geom_text(aes(x,y,label=label),data.frame(x=1,y=-0.6,label="Joins the left cluster on $\\alpha_1$ before joining right cluster."))+
-  geom_text(aes(x,y,label=label),data.frame(x=0.5,y=-0.1,label="Solution at $\\lambda=0.18$ yields 2 clusters."),colour="red")+
-  xlab("$\\alpha^2$")+
-  ylab("$\\alpha^1$")
-tikz("l1-2d.tex",width=11.5,pointsize=24)
+  geom_text(aes(x,y,label=label),data.frame(x=0.7,y=-0.6,label="Joins the left cluster on $\\alpha^1$\\ before joining right cluster."))+
+  geom_text(aes(x,y,label=label),data.frame(x=0.3,y=-0.1,label="Solution at $\\lambda=0.18$\\ yields 2 clusters."),colour="red")+
+  xlab("$\\alpha^2$\\hspace{2in}")+
+  ylab("$\\alpha^1$")+
+  mytheme
+tikz("l1-2d.tex",width=10,height=6,pointsize=24)
 print(p)
 dev.off()
 
 
 library(ggplot2)
-path.plot <- ggplot(path,aes(lambda,alpha))+
+toplot <- path
+levels(toplot$col) <- gsub("alpha.([12])","$\\\\alpha^\\1$",levels(path$col))
+path.plot <- ggplot(toplot,aes(lambda,alpha))+
   geom_line(aes(group=row))+
   geom_point(subset=.(lambda==0 & row==10))+
   geom_vline(xintercept=cutline,colour="red")+
-  facet_grid(col~.,scales="free")
+  facet_grid(col~.,scales="free")+
+  xlab("Location in the regularization path $\\lambda$")+
+  ylab("Optimal value of $\\ell_1$ clusterpath")+
+  mytheme+opts(axis.title.y=theme_text(vjust=-0.1,angle=90))
+tikz("l1-dims.tex",height=6,width=4)
 print(path.plot)
+dev.off()
+ 
 
